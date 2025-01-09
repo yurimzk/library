013b6bcaaa72d8087436c9a5cea7674b45b4566a
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-01-09 16:52:25 -0300

    import Book class,

---
+++ js/library.js
@@ -0,0 +1,18 @@
+import Book from '.Book.js';
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBookToLibrary(title, author, year, pages) {
+    const book = new Book(title, author, year, pages);
+    this.books.push(book);
+  }
+
+  displayBooks() {
+    console.log(this.books);
+  }
+}
+
+export default Library;

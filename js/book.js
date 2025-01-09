02a46523e3d6a9498013dc666174c77ccd53794c
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-01-09 16:48:03 -0300

    add and export Book class

---
+++ js/book.js
@@ -0,0 +1,10 @@
+class Book {
+  constructor(title, author, year, pages) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+  }
+}
+
+export default Book;

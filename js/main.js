9dec67815cb352ffcc6869930b9fd73540d4d056
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-01-09 20:14:07 -0300

    import Library to main.js and add books to library

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import Library from './Library.js';
+
+const myLibrary = new Library();
+myLibrary.addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 1937, 310);
+myLibrary.addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkien', 1954, 423);
+myLibrary.addBookToLibrary('To Kill a Mockinbird', 'Harper Lee', 1960, 281);

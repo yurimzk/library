f334bf051a567523435c6936baab30fba444de49
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-01-09 21:11:03 -0300

    add event listeners to toggle modal and to add new book to the table

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+import { myLibrary, displayBooks } from './main.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  function toggleModal() {
+    const modal = document.querySelector('.modal');
+    if (modal.open) {
+      modal.close();
+    } else {
+      modal.showModal();
+    }
+  }
+
+  document.querySelector('.btn-modal').addEventListener('click', toggleModal);
+
+  document.querySelector('#new-book-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const year = event.target.year.value;
+    const pages = event.target.pages.value;
+    myLibrary.addBookToLibrary(title, author, year, pages);
+    displayBooks();
+    toggleModal();
+  });
+});
